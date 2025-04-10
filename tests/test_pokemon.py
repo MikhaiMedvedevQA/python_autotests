0201c1376cef97b9f66e5b73b167b2000fbe1992
Mikhail Medvedev <cobra.mikhail@gmail.com>
2025-04-10 23:04:38 +0300

    Update test_pokemon.py

--- tests/test_pokemon.py
+++ tests/test_pokemon.py
@@ -16,12 +16,4 @@ def test_status_code():
     response=requests.get(url=f'{URL}/trainers/')
     assert response.status_code==200
 
-'''def test_part_of_response():
-    response_get=requests.get(url=f'{URL}/pokemons/', params={'trainer_id':TRAINER_ID})
-    assert response_get.json()['data'][0]['name']== 'mrGreen'
-
-@pytest.mark.parametrize ('key,value',[('name','mrGreen'), ('trainer_id',TRAINER_ID), ('id','285049')])
-def test_parametrize(key, value):
-    response_parametrize=requests.get(url=f'{URL}/pokemons/', params={'trainer_id':TRAINER_ID})
-    assert response_parametrize.json() ['data'][0][key] == value '''
 
